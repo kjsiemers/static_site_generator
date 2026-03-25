3d6e4388c2535a3f84a50c19b55a07f91f9c4e39
kjsiemers <kjs@umich.edu>
2026-03-25 16:09:48 -0400

    everything works as localhost site

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,5 +1,6 @@
 from markdown_blocks import markdown_to_html_node
 from htmlnode import *
+import os
 
 def extract_title(markdown):
     lines = markdown.splitlines()
@@ -23,3 +24,22 @@ def generate_page(from_path, template_path, dest_path):
     #print(f"Generated page content:\n{page_content}\n==========")
     with open(dest_path, 'w') as fo:
         fo.write(page_content)
+
+def generate_pages_recursive(src_dir, template_path, dest_dir):
+    if os.path.exists(src_dir):
+        files = os.listdir(src_dir)
+        if files is None or len(files) == 0:
+            return
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+        files = os.listdir(src_dir)
+        for file in files:
+            src_file_path = os.path.join(src_dir, file)
+            if os.path.isfile(src_file_path) and file.endswith(".md"):
+                dest_file_path = os.path.join(dest_dir, file[:-3] + ".html")
+                generate_page(src_file_path, template_path, dest_file_path)
+            elif os.path.isdir(src_file_path):
+                dest_file_path = os.path.join(dest_dir, file)
+                generate_pages_recursive(src_file_path, template_path, dest_file_path)
+    else:
+        print(f"'{src_dir}' directory does not exist.")
--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from copy_static import del_files_in_dir, copy_files
-from generate_page import generate_page
+from generate_page import generate_page, generate_pages_recursive
 
 def main():
     #print("Hello from static-site-generator!")
     del_files_in_dir("./public", [])
     copy_files("./static", "./public")
-    generate_page("./content/index.md", "./template.html", "./public/index.html")
-
+    #generate_page("./content/index.md", "./template.html", "./public/index.html")
+    generate_pages_recursive("./content", "./template.html", "./public")
 
 if __name__ == "__main__":
     main()
